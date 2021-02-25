4251a37dd19b6e797f2ff241f213fe79aaf4ab37
シロー <>
2021-02-25 06:47:47 +0000

    ruby03 kadai teisei

--- ruby_rensyu/if.rb
+++ ruby_rensyu/if.rb
@@ -4,13 +4,13 @@ if n == 3
   puts 'nは3です'
 end
 
-if n==3 || n==4
+if n == 3 || n == 4
   puts '3、4のどちらかです'
 end
 
-if n==3
+if n == 3
   puts 'nは3です'
-elsif n==4
+elsif n == 4
   puts 'nは４です'
 end
 
@@ -20,16 +20,16 @@ for num in 1..5 do
   puts hello
 end
   
-[1,2,3,4,5].each do |num|
+[1, 2, 3, 4, 5].each do |num|
   p num
 end
 
-if num ==2
+if num == 2
   puts "2です"
-  elsif num ==3
-    puts "3です"
-  else
-    puts "それ以外です"
-  end
+elsif num == 3
+  puts "3です"
+else
+  puts "それ以外です"
+end
   
   
