fdf1163d39492644d0092644a6d181fea3e3a12c
シロー <>
2021-02-25 06:42:39 +0000

    ruby02 kadai teisei

---
+++ ruby_rensyu/if.rb
@@ -0,0 +1,35 @@
+n = 3
+
+if n == 3
+  puts 'nは3です'
+end
+
+if n==3 || n==4
+  puts '3、4のどちらかです'
+end
+
+if n==3
+  puts 'nは3です'
+elsif n==4
+  puts 'nは４です'
+end
+
+num = 1
+hello = "こんにちは"
+for num in 1..5 do
+  puts hello
+end
+  
+[1,2,3,4,5].each do |num|
+  p num
+end
+
+if num ==2
+  puts "2です"
+  elsif num ==3
+    puts "3です"
+  else
+    puts "それ以外です"
+  end
+  
+  
--- ruby_rensyu/kadai1.rb
+++ ruby_rensyu/kadai1.rb
@@ -4,34 +4,34 @@ puts date
 two_lang = "Ruby" + "始めました"
 puts two_lang
 
-kazu = 3+3
-hiki = 3-3
-kake = 3*3
-wari = 3/3
+kazu = 3 + 3
+hiki = 3 - 3
+kake = 3 * 3
+wari = 3 / 3
 puts kazu
 puts hiki
 puts kake
 puts wari
 
-array = [1,2,3,4]
+array = [1, 2, 3, 4]
 puts array
 
 num_array = array
 puts num_array
 
-takuya = {name:'hamada_shiro',birthday:'1990/06/20',blood_type:"O" }
+takuya = {name: 'hamada_shiro', birthday: '1990/06/20', blood_type: "O" }
 p takuya[:birthday]
 
 def plus_ruby(plus)
-  puts "#{plus}ruby" 
+  plus + "ruby" 
 end
 
-plus_ruby("like")
-plus_ruby("yhee")
+p plus_ruby("like")
+p plus_ruby("yhee")
 
 
 def plus_one(suti)
-  puts suti + 1
+  suti + 1
 end
 
 p plus_one(4)
